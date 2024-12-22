a9814bfc3e95bb1be171493aadb6f7846e60305a
s-v-akinfiev <s-v-akinfiev@users.noreply.github.com>
2024-12-22 16:51:58 +0200

    Removed unused instructions

--- ubuntu_install_script.sh
+++
@@ -1,4 +0,0 @@
-sudo service docker stop
-sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
-reboot
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin=2.17.3-1~ubuntu.22.04~jammy
