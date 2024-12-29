c33d60819c0b9134ec159b9819785b826a75c968
s-v-akinfiev <s-v-akinfiev@users.noreply.github.com>
2024-12-29 18:02:13 +0200

    Removed dolphin uninstallation from instruction

--- instructions/kde.sh
+++ instructions/kde.sh
@@ -10,7 +10,6 @@ run() {
     sudo apt install -y kde-plasma-desktop
     sudo apt install -y kubuntu-desktop
     sudo apt remove -y ark gnome-characters gnome-clocks elisa kcharselect \
-        dolphin dolphin-plugins gwenview haruna kinfocenter khelpcenter \
-        kate krdc kdeconnect ksystemlog kfind konversation okular \
-        okular-extra-backends neochat seahorse skanlite skanpage
+        gwenview haruna kinfocenter khelpcenter kate krdc kdeconnect ksystemlog \
+        kfind konversation okular okular-extra-backends neochat seahorse skanlite skanpage
 }
